796818aa0f8bd12a854a281e89cf8211e1e66515
Thiago Almeida <contatothiagomoreira@outlook.com>
2021-11-17 22:16:24 -0300

    modifications layout

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const menu = document.querySelector('.nav-list');
 const btnMenu = document.querySelector('.btn-menu');
 const iconMenu = document.querySelector('.btn-menu img');
+const links = document.querySelectorAll('.nav-list a');
 
 function handleClick() {
   menu.classList.toggle('active');
@@ -14,3 +15,9 @@ function handleClick() {
 }
 
 btnMenu.addEventListener('click', handleClick);
+
+console.log(links)
+
+links.forEach(link => {
+  link.addEventListener('click', handleClick);
+})
